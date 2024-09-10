c414531b9493a88b7c296a18ffb82cf59e4b6be3
Andris2579 <andrewchren1@gmail.com>
2024-09-10 15:55:18 +0200

    Projekt V1

---
+++ src/Database/DB.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Database;
+class DB{
+    const HOST = 'localhost';
+    const USER = 'root';
+    const PASSWORD = null;
+    const DATABASE = 'postoffice';
+        protected $mysqli;
+        function __construct($host = 'localhost', $user = 'root', $password = null, $database = 'projekt'){
+            $this->mysqli = mysqli_connect($host, $user, $password, $database);
+
+            if(!$this->mísqli){
+                die("Connection failed: ".mysqli_connect_error());
+            }
+            $this->mysqli->set_charset("utf8mb4");
+
+            function __destruct(){
+                $this->mysqli->close();
+            }
+        }
+    }
+?>
